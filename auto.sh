ec9b2ed51037886413cc54e5c139557fbbd3a864
lows93 <lqwerti93@gmail.com>
2018-12-04 23:39:05 +0100

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -3,9 +3,12 @@ while true
   do
     STEAK=`gaiacli query account --chain-id=gaia-9002 cosmos1qm7knjdln0ap9fpykdu5rcajm9cf2esvp8ey2v --trust-node$
     passphrase="enter your passphrase here from your account as an string"
-    echo ""
     echo "$((STEAK - 1))"
     Zahl="$((STEAK - 1))"
     echo $passphrase|gaiacli tx stake delegate --from "main" --validator "cosmosvaloper1qm7knjdln0ap9fpykdu5rcaj$
-    sleep 30s
-  done
+    sleep 10s
+    echo ""
+    gaiacli status | jq ".[].voting_power"
+    echo ""
+    sleep 2s
+   done

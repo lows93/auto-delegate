9e5011dec49b33c0f4dbc76ff5dabbdde7996f4e
lows93 <lqwerti93@gmail.com>
2018-12-08 19:12:02 +0100

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 while true
-  do
-    STEAK=`gaiacli query account --chain-id=gaia-9002 cosmos1qm7knjdln0ap9fpykdu5rcajm9cf2esvp8ey2v --trust-node$
-    passphrase="enter your passphrase here from your account as an string"
-    echo "Got ""$((STEAK - 1))"" Reward-STAKE"
-    Zahl="$((STEAK - 1))"
-    echo $passphrase|gaiacli tx stake delegate --from "main" --validator "cosmosvaloper1qm7knjdln0ap9fpykdu5rcaj$
-    sleep 10s
-    echo ""
-    gaiacli status | jq ".[].voting_power"
-    echo ""
-    sleep 2s
-   done
+        do
+                STEAK=`gaiacli query account --chain-id=genki-1000 cosmos1qm7knjdln0ap9fpykdu5rcajm9cf2esvp8ey2v --trust-node | jq ".value.coins" | jq ".[0].amount" | bc`
+                passphrase=""
+                echo ""
+                echo "Got ""$((STEAK - 1))"" Reward-STAKE"
+                Zahl="$((STEAK - 1))"
+                echo $passphrase|gaiacli tx stake delegate --from "main" --validator "cosmosvaloper1qm7knjdln0ap9fpykdu5rcajm9cf2esvynd3xl" --chain-id "genki-1000" --amount "$Zahl""STAKE" 
+                sleep 6s
+                echo ""
+                echo ""
+                gaiacli status | jq ".[].voting_power"
+                echo ""
+                echo ""
+                sleep 4s
+        done

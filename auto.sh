866f171af1deb1da3d6397693b86e3d0f98695c4
lows93 <lqwerti93@gmail.com>
2018-12-10 18:34:56 +0100

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -2,20 +2,25 @@
 # Script For Auto-Delegate from validator kreios 
 while true
         do
-                #replace cosmos1qm7knjdln0ap9fpykdu5rcajm9cf2esvp8ey2v with your own cosmosaddr
-                STEAK=`gaiacli query account --chain-id=genki-2000 cosmos1qm7knjdln0ap9fpykdu5rcajm9cf2esvp8ey2v --trust-node | jq ".value.coins" | jq ".[0].amount" | bc`
                 passphrase=""
+                echo $passphrase|gaiacli tx dist withdraw-rewards --chain-id "genki-2000" --from "main" --is-validator
+                STEAK=`gaiacli query account --chain-id=genki-2000 cosmos1qm7knjdln0ap9fpykdu5rcajm9cf2esvp8ey2v --trust-node | jq ".value.coins" | jq ".[0].amount" | bc`
+                while [ "$STEAK" != "0" ]
+                        do
+                                echo ""
+                                echo "Got ""$((STEAK ))"" Reward-STAKE"
+                                echo ""
+                                echo $passphrase|gaiacli tx dist withdraw-rewards --chain-id "genki-2000" --from "main" --is-validator
+                                echo $passphrase|gaiacli tx stake delegate --from "main" --validator "cosmosvaloper1qm7knjdln0ap9fpykdu5rcajm9cf2esvynd3xl" --chain-id "genki-2000" --amount "$STEAK""STAKE" 
+                                sleep 10s
+                                echo ""
+                                gaiacli status | jq ".[].voting_power"
+                                echo ""
+                                sleep 11s
+                done
+                echo""
+                echo "You only have $STEAK STAKE"
                 echo ""
-                echo "Got ""$((STEAK - 1))"" Reward-STAKE"
-                Zahl="$((STEAK - 1))"
-                #replace --from "main" with your own keyname and cosmosvaloper1qm7knjdln0ap9fpykdu5rcajm9cf2esvynd3xl with the output from gaiacli keys show yourkeyname --bech=val
-
-                echo $passphrase|gaiacli tx stake delegate --from "main" --validator "cosmosvaloper1qm7knjdln0ap9fpykdu5rcajm9cf2esvynd3xl" --chain-id "genki-2000" --amount "$Zahl""STAKE" 
-                sleep 6s
-                echo ""
-                echo ""
-                gaiacli status | jq ".[].voting_power"
-                echo ""
-                echo ""
-                sleep 4s
+                sleep 12000
         done
+
